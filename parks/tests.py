e07ed0759b6e0e2d978be06a475848d5bba04fdb
IanC13 <ian.yl.cheng@gmail.com>
2025-03-12 19:00:21 -0400

    Add park detail test

--- parks/tests.py
+++ parks/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 from .models import DogRun
+from parks.utilities import folium_cluster_styling
 
 
 class ParkModelTest(TestCase):
@@ -80,3 +81,34 @@ class CombinedViewTest(TestCase):
     def test_combined_view(self):
         response = self.client.get(reverse("park_and_map"))
         self.assertEqual(response.status_code, 200)
+
+class ParkDetailViewTest(TestCase):
+    def test_park_detail_not_found(self):
+            """Test accessing a non-existent park should return 404."""
+            response = self.client.get(reverse("park_detail", args=[9999]))  # Non-existent ID
+            self.assertEqual(response.status_code, 404)
+    
+
+
+    def test_get_park_detail(self):
+        """Set up a test park object."""
+        self.client = Client()
+
+        self.park = DogRun.objects.create(
+            id="1",
+            prop_id="1234",
+            name="Central Park",
+            address="New York, NY",
+            dogruns_type="Small",
+            accessible="Yes",
+            notes="Test park notes",
+            image=None,
+        )
+
+
+        self.park_detail_url = reverse("park_detail", args=[self.park.id])
+        """Test retrieving the park detail page."""
+        response = self.client.get(self.park_detail_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "parks/park_detail.html")
+        self.assertContains(response, self.park.name)
