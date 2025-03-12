117a0bafdebcf9b6af92779f5d07fc09f0003bbc
Marvin Ren <marvin.ren544@gmail.com>
2025-03-12 04:24:48 -0400

    3

--- parks/models.py
+++ parks/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class DogRun(models.Model):
-    # id = models.CharField(max_length=255)
     id = models.AutoField(primary_key=True)
     prop_id = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
