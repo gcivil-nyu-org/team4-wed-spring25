3571e86ca0681e65c52a1d9196eea037dc142ac0
Marvin Ren <marvin.ren544@gmail.com>
2025-03-12 02:16:12 -0400

    tests for health check

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.hello_world, name="Home")]
+urlpatterns = [
+    path("health_check/", views.health_check, name="health_check"),
+    # path('test_db_connection/', views.test_db_connection, name='test_db_connection'),
+    # path('hello_world/', views.hello_world, name='hello_world'),
+    # path('', views.hello_world, name='Home'),
+]
--- homepage/views.py
+++ homepage/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import render  # noqa: F401  # Ignore "imported but unused"
-from django.http import HttpResponse
+# from django.shortcuts import render  # noqa: F401  # Ignore "imported but unused"
+# from django.http import HttpResponse
 from django.http import JsonResponse
-from django.db import connection
 
+# from django.db import connection
 
-def test_db_connection(request):
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT 1;")
-        return JsonResponse({"status": "success", "message": "Database connected!"})
-    except Exception as e:
-        return JsonResponse({"status": "error", "message": str(e)}, status=500)
+# def test_db_connection(request):
+#     try:
+#         with connection.cursor() as cursor:
+#             cursor.execute("SELECT 1;")
+#         return JsonResponse({"status": "success", "message": "Database connected!"})
+#     except Exception as e:
+#         return JsonResponse({"status": "error", "message": str(e)}, status=500)
 
 
 def health_check(request):
     return JsonResponse({"status": "ok"})
 
 
-def hello_world(request):
-    return HttpResponse("Hello World")
+# def hello_world(request):
+#     return HttpResponse("Hello World")
