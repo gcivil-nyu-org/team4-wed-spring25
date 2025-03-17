d8e3c82eae8455720679de23ba03a9004da307c2
Kumuda Aggarwal <kaggarwal@natera.com>
2025-03-17 05:22:07 -0400

    Adding bootstrap

--- parks/models.py
+++ parks/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
-
 class DogRun(models.Model):
     id = models.CharField(max_length=255)
     prop_id = models.CharField(max_length=255, primary_key=True)
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
-    dogruns_type = models.CharField(max_length=100)  # Fix here
+    dogruns_type = models.CharField(max_length=100)  
     accessible = models.CharField(max_length=50)
     notes = models.TextField(max_length=255)
-    image = models.ImageField(upload_to="dogruns/", null=True, blank=True)  # New field
+    image = models.ImageField(upload_to='dogruns/', null=True, blank=True)  
 
     class Meta:
         db_table = "dog_runs"
 
     def __str__(self):
         return self.name
+    
+
