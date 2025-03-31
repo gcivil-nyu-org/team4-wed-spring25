b0338d5efb49a96773bbed058bc4687f0370a349
07653 <1449217600@qq.com>
2025-03-31 12:18:22 -0400

    adding static_root

--- pawpark/settings.py
+++ pawpark/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # STATIC_URL = "static/"
 # STATIC_ROOT = 'static'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
