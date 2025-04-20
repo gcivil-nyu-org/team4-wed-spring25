14c7e1fd5eb50d18d35d6676cfbb3da788523d6e
Kumuda Aggarwal <kaggarwal@natera.com>
2025-04-19 21:48:02 -0400

    AWS deployments

--- pawpark/settings.py
+++ pawpark/settings.py
@@ -60,14 +60,20 @@ INSTALLED_APPS = [
 
 ASGI_APPLICATION = "pawpark.asgi.application"
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
-    },
-}
+USE_REDIS = os.getenv("USE_REDIS", "true").lower() == "true"
+REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")  # Default to local, override in AWS
+
+if USE_REDIS:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [(REDIS_HOST, 6379)],
+            },
+        }
+    }
+else:
+    CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}}
 
 
 MIDDLEWARE = [
